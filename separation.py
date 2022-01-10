5f581e1db56d6c649d27ab545f7bc84175dc4fc9
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-10 10:08:59 +0100

    omxplayer

--- separation.py
+++ separation.py
@@ -1,4 +1,11 @@
+from omxplayer.player import OMXPlayer
 from bluepy.btle import Scanner
+from pathlib import Path 
+import _thread 
+
+#loop = OMXPlayer( Path( '/home/pi/separation_videos/mother.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.loop' )
+#loop.set_volume( 0 )
+
 
 while True:
     try:
