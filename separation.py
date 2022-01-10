3db497592cb3ca5c37240e8cb5dac8c8a73c4e4c
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-10 10:40:55 +0100

    sam small

--- separation.py
+++ separation.py
@@ -1,17 +1,15 @@
 from omxplayer.player import OMXPlayer
 from bluepy.btle import Scanner
-from pathlib import Path 
+from pathlib import Path
 import _thread 
 
 #loop = OMXPlayer( Path( '/home/pi/separation_videos/mother.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.loop' )
 #loop.set_volume( 0 )
 
-
 while True:
     try:
-        #10.0 sec scanning
-        ble_list = Scanner().scan(10.0)
+        ble_list = Scanner().scan( 10.0 ) #10.0 sec scanning 
         for dev in ble_list:
-            print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+            print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
     except:
-        raise Exception("Error occured")
+        raise Exception( "Error occured" )
