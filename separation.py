0f1d4ebf680dd5d0d933a185c3249ace0cbed238
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-11 17:41:50 +0100

    scan

--- separation.py
+++ separation.py
@@ -3,10 +3,10 @@ from bluepy.btle import Scanner
 from pathlib import Path
 import _thread 
 
-calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
-calm.set_volume( 0 )
-ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
-ancious.set_volume( 0 )
+#calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
+#calm.set_volume( 0 )
+#ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
+#ancious.set_volume( 0 )
 
 rssi_average = 1
 rssi_average_list = []
@@ -18,7 +18,7 @@ def rssi_scanner( address ):
             if len( rssi_average_list ) > 10:
                 rssi_average_list.pop( 0 )
             rssi_average = ( float( sum( rssi_average_list ) ) / len( rssi_average_list ) )
-        #print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
+        print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
 
 _thread.start_new_thread( rssi_scanner, ('B8:27:EB:06:DF:94') ) 
 
