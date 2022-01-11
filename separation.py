ea10e7a29a276314707a5764e18fd8707594ee10
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-11 18:25:22 +0100

    s

--- separation.py
+++ separation.py
@@ -1,5 +1,6 @@
 from omxplayer.player import OMXPlayer
 from bluepy.btle import Scanner
+from bt_proximity import BluetoothRSSI
 from pathlib import Path
 import _thread 
 
@@ -8,18 +9,20 @@ import _thread
 #ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
 #ancious.set_volume( 0 )
 
+BT_ADDR = 'B8:27:EB:06:DF:94'
+btrssi = BluetoothRSSI(addr=BT_ADDR)
+
 rssi_average = 1
 rssi_average_list = []
 def rssi_scanner( address ):
+    btrssi = BluetoothRSSI( addr=address )
     while True:
-        ble_list = Scanner().scan( 1.0 ) #10.0 sec scanning 
-        for dev in ble_list:
-            if dev.addr == address:
-                rssi_average_list.append( dev.rssi )
-                if len( rssi_average_list ) > 10:
-                    rssi_average_list.pop( 0 )
-                rssi_average = ( float( sum( rssi_average_list ) ) / len( rssi_average_list ) )
-                print( rssi_average )
+            rssi_average_list.append(  btrssi.get_rssi() )
+            if len( rssi_average_list ) > 10:
+                rssi_average_list.pop( 0 )
+            rssi_average = ( float( sum( rssi_average_list ) ) / len( rssi_average_list ) )
+            print( rssi_average )
+            sleep( 1 )
             #print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
 
 _thread.start_new_thread( rssi_scanner, ('74:38:b7:cf:eb:f6', ) ) 
