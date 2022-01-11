70c54b527e65a58adbd2e3791aa21fa04649a6f8
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-11 18:39:48 +0100

    t1

--- separation.py
+++ separation.py
@@ -3,10 +3,10 @@ from bluepy.btle import Scanner
 from pathlib import Path
 import _thread 
 
-#calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
-#calm.set_volume( 0 )
-#ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
-#ancious.set_volume( 0 )
+calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
+calm.set_volume( 0 )
+ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
+ancious.set_volume( 0 )
 
 rssi_average = 1
 rssi_average_list = []
@@ -29,13 +29,13 @@ while True:
     try:
         #device_is_close = rssi_average > -3
         #device_is_far = rssi_average < -6
-        if rssi_average < -6:
-            #ancious.set_alpha( 255 )
-            #ancious.set_volume( 100 )
+        if rssi_average < -60:
+            ancious.set_alpha( 255 )
+            ancious.set_volume( 100 )
             pass
-        if rssi_average > -3:
-            #ancious.set_alpha( 255 )
-            #ancious.set_volume( 100 )
+        if rssi_average > -40:
+            ancious.set_alpha( 255 )
+            ancious.set_volume( 100 )
             pass
     except:
         raise Exception( "Error occured" )
