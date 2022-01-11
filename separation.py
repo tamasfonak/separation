cec2519630573d14a4d1787c5aa2b021142788d0
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-11 19:08:36 +0100

    t13

--- separation.py
+++ separation.py
@@ -3,8 +3,8 @@ from bluepy.btle import Scanner
 from pathlib import Path
 import _thread 
 
-#calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
-#calm.set_volume( 0 )
+calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
+calm.set_volume( 0 )
 ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '1', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
 ancious.set_volume( 0 )
 
@@ -17,7 +17,7 @@ def rssi_scanner( address ):
         for dev in ble_list:
             if dev.addr == address:
                 rssi_average_list.append( dev.rssi )
-                if len( rssi_average_list ) > 10:
+                if len( rssi_average_list ) > 5:
                     rssi_average_list.pop( 0 )
                 rssi_average = ( float( sum( rssi_average_list ) ) / len( rssi_average_list ) )
                 print( abs(rssi_average) )
@@ -29,11 +29,9 @@ _thread.start_new_thread( rssi_scanner, ('7e:17:54:25:08:54', ) )
 while True:
     try:
         if (abs(rssi_average) > 50.0 ):
-            print( 'ancious' )
             ancious.set_alpha( 255 )
             ancious.set_volume( 1 )
         if (abs(rssi_average) < 40.0 ):
-            print( 'calm' )
             ancious.set_alpha( 0 )
             ancious.set_volume( 0 )
     except:
