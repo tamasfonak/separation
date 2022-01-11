615acb0eb2aad8e736e7c38975677b435b133c36
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-11 18:57:55 +0100

    t13

--- separation.py
+++ separation.py
@@ -29,11 +29,11 @@ while True:
     try:
         #device_is_close = rssi_average > -3
         #device_is_far = rssi_average < -6
-        if rssi_average < -50:
+        if (rssi_average < -50):
             print( 'ancious' )
             ancious.set_alpha( 255 )
             ancious.set_volume( 1 )
-        if rssi_average > -40:
+        if (rssi_average > -40):
             print( 'calm' )
             ancious.set_alpha( 0 )
             ancious.set_volume( 0 )
