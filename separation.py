b80bc5082e3c213b6ba237d5df886da42c200cd3
Tamas Berces <tamasfonak@users.noreply.github.com>
2022-01-17 17:43:24 +0100

    some mods

--- separation.py
+++ separation.py
@@ -4,11 +4,10 @@ from pathlib import Path
 import time
 import _thread
 
-ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '1', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
-ancious.set_volume( 0 )
 calm = OMXPlayer( Path( '/home/pi/separation_video/baba_01.mp4' ), args = [ '--no-osd', '--loop', '--layer', '0', '--win', '0,0,1920,1080', '--alpha', '1' ], dbus_name = 'org.mpris.MediaPlayer2.calm' )
 calm.set_volume( 0 )
-
+ancious = OMXPlayer( Path( '/home/pi/separation_video/baba_02.mp4' ), args = [ '--no-osd', '--loop', '--layer', '1', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name = 'org.mpris.MediaPlayer2.ancious' )
+ancious.set_volume( 0 )
 
 rssi_average = 1
 rssi_average_list = []
@@ -23,7 +22,7 @@ def rssi_scanner( address ):
                     rssi_average_list.pop( 0 )
                 rssi_average = ( float( sum( rssi_average_list ) ) / len( rssi_average_list ) )
                 print( abs(rssi_average) )
-            print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
+            #print( "rssi: {} ; mac: {}".format( dev.rssi, dev.addr ) )
 
 _thread.start_new_thread( rssi_scanner, ('53:1a:42:67:24:56', ) ) 
 
